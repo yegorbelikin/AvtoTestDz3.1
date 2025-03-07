df06721ae91b1ef2651cd0ea080e68c555bdfd52
Yegor <yegor.belikin@gmail.com>
2025-03-07 16:39:37 +0500

    Corrected Alfacard test

--- src/test/java/ru/netology/AlfacardTest.java
+++ src/test/java/ru/netology/AlfacardTest.java
@@ -28,7 +28,7 @@ public class AlfacardTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
